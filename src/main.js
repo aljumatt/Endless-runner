25fbb7a57a457058dd24607199ac92f272a87cab
z1ord <zlordnumber1@gmail.com>
2026-02-19 15:55:51 -0600

    intial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//Alekzander Matthews
+
+
+//create game object
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+let game = new Phaser.Game(config)
